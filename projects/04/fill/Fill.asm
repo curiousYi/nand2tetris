778a14ef8c5ae84edeee1e80573f757107e0c39d
Yi Chao <changingtimes@gmail.com>
2017-12-03 03:34:19 -0600

    Had issues writing the pointers. Did so after reading this SO post, but still fully don't understand the top upvoted answer.
    https://stackoverflow.com/questions/13654066/nand-2-tetris-asm-expression-expected

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -10,5 +10,45 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
+  @16384 //initializing iterator?
+  D=A //beginning of the Hack Computer Screen
 
-// Put your code here.
+  @StartingPoint
+  M=D;
+
+  @24576
+  D=A
+
+  @KeyboardInput
+  M=D  //HackKeyboard single-world key map RAM[24576]
+
+  @KeyboardInput
+  D=M
+
+  @2 //just go to the end of the loop for now  
+  @KeyboardInput; JEQ //equal 0 skip filling //with black characters?
+
+  @startingPoint
+  D=M
+
+  @i
+  A=D+M
+  M=-1
+
+  @i
+  M=M+16
+
+  @i
+  D=M-16896
+
+  @25 // done looping
+  D;JGE
+
+  @KeyboardInput
+  M=D
+
+  @47
+  D;JNE
+
+  @11
+  0;JMP //go back to beginning for infinite loop
